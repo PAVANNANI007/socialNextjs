17e24585bffc3150868c0649adca1f8d01d83609
DODIYAM PAVAN KUMAR <68941005+PAVANNANI007@users.noreply.github.com>
2024-06-12 00:34:51 +0530

    output

---
+++ Frontend/app/(routes)/_components/Header.js
@@ -0,0 +1,22 @@
+import { Button } from '@/components/ui/button'
+import { Menu, MenuSquare } from 'lucide-react'
+import React from 'react'
+import Link from 'next/link'
+
+function Header({toggleSideBar}) {
+  return (
+    <div className='p-5 flex justify-between md:justify-end shadow-sm bg-white items-center'>
+        <Menu className='md:hidden h-7 w-7
+         text-slate-500 cursor-pointer'
+         onClick={()=>toggleSideBar()}
+         />
+         <Link href='/sign-in'>
+        <Button className='bg-blue-500 
+        hover:bg-blue-600 shadow-sm'>
+            Get Started</Button>
+            </Link>
+    </div>
+  )
+}
+
+export default Header
---
+++ Frontend/app/page.js
@@ -0,0 +1,14 @@
+"use client"
+import Image from 'next/image'
+import { useEffect } from 'react';
+import GlobalApi from './_utils/GlobalApi';
+import { UserButton, useUser } from '@clerk/nextjs';
+
+export default function Home() {
+  
+  return (
+  <div>
+  <UserButton/>
+  </div>
+  )
+}
